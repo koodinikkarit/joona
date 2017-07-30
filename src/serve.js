accf458347e195f825b1b0710550dffcc2313c6d
jaska <pusanteemu@gmail.com>
2017-07-30 14:44:34 +0300

    Muutettu ssr serve.

--- src/serve.js
+++ src/serve.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOMServer from "react-dom/server";
 import request from "request";
 import PageFrame from "./js/PageFrame";
+import read from "read-yaml";
 
-export default function ssr(app) {
+const config = read.sync("config.yml");
+
+export default (app) => {
 	app.get("/login", (req, res) => {
 		res.redirect(302, "http://localhost:7070?return_url=http://localhost:11111/create_session");
 	});
@@ -14,20 +17,30 @@ export default function ssr(app) {
         res.redirect(302, "http://localhost:11111");
     });
 
-    app.use("/api", (req, res) => {
-        request.post("http://localhost:9595/", { form: req.body}, (err, data) => {
-            if (!data) {
-                console.log("500 service unvaivable");
-                res.writeHead(500);
-            } else {
-                res.end(data.body);
-            }
-        });
-    });
+	if (config.petriIp && config.petriPort) {
+		app.use("/api", (req, res) => {
+			request.post(`http://${config.petriIp}:${config.petriPort}/`, { form: req.body }, (err, data) => {
+				if (!data) {
+					console.log("500 service unvaivable");
+					res.writeHead(500);
+				} else {
+					res.end(data.body);
+				}
+			});
+		});
+	} else {
+		app.use("/api", (req, res) => {
+			res.writeHead(500);
+		});
+	}
 
     app.use((req, res) => {
 		res.status(200);
         res.send(`<!doctype html>\n${ReactDOMServer.renderToString(<PageFrame />)}`);
         res.end();
-    });
+	});
+	
+	app.listen(config.port, () => {
+    	console.log("serveri on käynnissä");
+	});
 }
