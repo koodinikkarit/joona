04dc85116ce96d099035263fe053b2021564b823
Jaska <pusanteemu@gmail.com>
2017-08-25 08:57:37 +0300

    webpack config muutoksia.

--- webpack.analyze.config.js
+++ webpack.analyze.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 const moduleConfig = require(path.resolve(__dirname, "src", "module-config"));
 
 module.exports = {
@@ -6,8 +8,13 @@ module.exports = {
 	entry: {
 		app: path.resolve(__dirname, "src/js/", "app.js")
 	},
+	plugins: [
+		new BundleAnalyzerPlugin({
+			analyzerPort: 4567
+		})
+	],
 	output: {
 		path: path.resolve(__dirname, "dist"),
 		filename: "app.js"
-	}	
+	}
 };
