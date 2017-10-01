2f0cf41a7d52707add5e81612fef10234d4e7a63
Jaska <pusanteemu@gmail.com>
2017-10-01 21:57:21 +0300

    Lisätty id luonti undefined id tilalle.

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,12 @@
 import ApolloClient, { createNetworkInterface } from "apollo-client";
 //import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws';
 
+let nextId = 1;
+
 export default new ApolloClient({
 	dataIdFromObject: o => {
 		//console.log("o", o);
-		return `${o.__typename}-${o.id},`;		
+		return `${o.__typename}-${o.id ? o.id : nextId++},`;		
 	},
 	networkInterface: createNetworkInterface({ 
 		uri: `http://${location.hostname}:${location.port}/api`,
