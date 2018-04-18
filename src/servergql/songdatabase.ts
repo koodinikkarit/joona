8355acac188c16b0e9870291bb329541dddc2820
Jaska <pusanteemu@gmail.com>
2018-04-18 22:50:02 +0300

    Luotu Laulutietokanta laulut komponentti.

--- src/servergql/songdatabase.ts
+++ src/servergql/songdatabase.ts
@@ -20,3 +20,39 @@ export const SONG_DATABASE_QUERY = gql`
 		}
 	}
 `;
+
+export const ADD_VARIATION_TO_SONG_DATABASE_MUTATION = gql`
+	mutation addVariationToSongDatabase(
+		$songDatabaseId: ID!
+		$variationId: ID!
+	) {
+		addVariationToSongDatabase(
+			songDatabaseId: $songDatabaseId
+			variationId: $variationId
+		)
+	}
+`;
+
+export const REMOVE_VARIATION_FROM_SONG_DATABASE_MUTATION = gql`
+	mutation removeVariationFromSongDatabase(
+		$songDatabaseId: ID!
+		$variationId: ID!
+	) {
+		removeVariationFromSongDatabase(
+			songDatabaseId: $songDatabaseId
+			variationId: $variationId
+		)
+	}
+`;
+
+export const SONG_DATABASE_VARIATIONS_QUERY = gql`
+	query getSongDatabaseVariations($songDatabaseId: ID!) {
+		songDatabaseVariations(songDatabaseId: $songDatabaseId) {
+			totalCount
+			variations {
+				id
+				name
+			}
+		}
+	}
+`;
--- src/songdatabases/SongDatabaseVariations.tsx
+++ src/songdatabases/SongDatabaseVariations.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { Panel } from "react-bootstrap";
+
+import { Query, QueryResult } from "react-apollo";
+
+import { Panel, ListGroup, ListGroupItem } from "react-bootstrap";
+import { SONG_DATABASE_VARIATIONS_QUERY } from "../servergql/songdatabase";
+
+import {
+	getSongDatabaseVariationsQuery,
+	getSongDatabaseVariationsQueryVariables
+} from "../types";
 
 export const SongDatabaseVariations = (inputProps: {
 	songDatabaseId: string;
@@ -7,7 +16,35 @@ export const SongDatabaseVariations = (inputProps: {
 	return (
 		<Panel>
 			<Panel.Heading>Laulutietokannan laulut</Panel.Heading>
-			<Panel.Body>asd</Panel.Body>
+			<Panel.Body>
+				<Query
+					query={SONG_DATABASE_VARIATIONS_QUERY}
+					variables={{ songDatabaseId: inputProps.songDatabaseId }}
+				>
+					{(
+						props: QueryResult<
+							getSongDatabaseVariationsQuery,
+							getSongDatabaseVariationsQueryVariables
+						>
+					) => {
+						if (props.loading || props.error) {
+							return <div />;
+						}
+
+						return (
+							<ListGroup>
+								{props.data.songDatabaseVariations.variations.map(
+									p => (
+										<ListGroupItem key={p.id}>
+											{p.name}
+										</ListGroupItem>
+									)
+								)}
+							</ListGroup>
+						);
+					}}
+				</Query>
+			</Panel.Body>
 		</Panel>
 	);
 };
