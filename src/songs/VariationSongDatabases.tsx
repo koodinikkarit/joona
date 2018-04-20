710e20aa6014d0f96e078d427d10aa3bb52006ae
Jaska <pusanteemu@gmail.com>
2018-04-20 18:17:26 +0300

    Lisätty variaatiotietokannan komponenttiin laulujentietokantojen lisäys.

--- src/songs/VariationSongDatabases.tsx
+++ src/songs/VariationSongDatabases.tsx
@@ -1,15 +1,29 @@
 import * as React from "react";
 
-import { Query, QueryResult } from "react-apollo";
+import {
+	Query,
+	QueryResult,
+	Mutation,
+	MutationOptions,
+	FetchResult
+} from "react-apollo";
 
 import { Panel } from "react-bootstrap";
 import {
 	getVariationSongDatabasesQuery,
 	getVariationSongDatabasesQueryVariables,
-	getSongDatabasesQuery
+	getSongDatabasesQuery,
+	addVariationToSongDatabaseMutation,
+	addVariationToSongDatabaseMutationVariables,
+	removeVariationFromSongDatabaseMutationVariables,
+	removeVariationFromSongDatabaseMutation
 } from "../types";
 import { VARIATION_SONG_DATABASES_QUERY } from "../servergql";
-import { SONG_DATABASES_QUERY } from "../servergql/songdatabase";
+import {
+	SONG_DATABASES_QUERY,
+	ADD_VARIATION_TO_SONG_DATABASE_MUTATION,
+	REMOVE_VARIATION_FROM_SONG_DATABASE_MUTATION
+} from "../servergql/songdatabase";
 
 export const VariationSongDatabases = (inputProps: { variationId: string }) => {
 	return (
@@ -40,24 +54,177 @@ export const VariationSongDatabases = (inputProps: { variationId: string }) => {
 								}
 
 								return (
-									<div>
-										{props2.data.searchSongDatabases.songDatabases.map(
-											p => {
-												const selected = props.data.variationSongDatabases.songDatabases.some(
-													e => e.id === p.id
-												);
-												return (
-													<div key={p.id}>
-														<input
-															type="checkbox"
-															checked={selected}
-														/>
-														{" " + p.name}
+									<Mutation
+										mutation={
+											ADD_VARIATION_TO_SONG_DATABASE_MUTATION
+										}
+									>
+										{(
+											addVariationToSongDatabase: (
+												props: MutationOptions<
+													addVariationToSongDatabaseMutation,
+													addVariationToSongDatabaseMutationVariables
+												>
+											) => Promise<
+												FetchResult<
+													addVariationToSongDatabaseMutation
+												>
+											>
+										) => (
+											<Mutation
+												mutation={
+													REMOVE_VARIATION_FROM_SONG_DATABASE_MUTATION
+												}
+											>
+												{(
+													removeVariationFromSongDatabase: (
+														props: MutationOptions<
+															removeVariationFromSongDatabaseMutation,
+															removeVariationFromSongDatabaseMutationVariables
+														>
+													) => Promise<
+														FetchResult<
+															removeVariationFromSongDatabaseMutation
+														>
+													>
+												) => (
+													<div>
+														{props2.data.searchSongDatabases.songDatabases.map(
+															p => {
+																const selected = props.data.variationSongDatabases.songDatabases.some(
+																	e =>
+																		e.id ===
+																		p.id
+																);
+																return (
+																	<div
+																		key={
+																			p.id
+																		}
+																	>
+																		<input
+																			type="checkbox"
+																			checked={
+																				selected
+																			}
+																			onClick={() => {
+																				if (
+																					selected
+																				) {
+																					removeVariationFromSongDatabase(
+																						{
+																							variables: {
+																								songDatabaseId:
+																									p.id,
+																								variationId:
+																									inputProps.variationId
+																							},
+																							update: (
+																								cache,
+																								res: FetchResult<
+																									removeVariationFromSongDatabaseMutation
+																								>
+																							) => {
+																								if (
+																									res
+																										.data
+																										.removeVariationFromSongDatabase
+																								) {
+																									const variationSongDatabases: getVariationSongDatabasesQuery = cache.readQuery(
+																										{
+																											query: VARIATION_SONG_DATABASES_QUERY,
+																											variables: {
+																												variationId:
+																													inputProps.variationId
+																											}
+																										}
+																									);
+
+																									const c =
+																										variationSongDatabases.variationSongDatabases;
+
+																									c.songDatabases = c.songDatabases.filter(
+																										e =>
+																											e.id !==
+																											p.id
+																									);
+
+																									cache.writeQuery(
+																										{
+																											query: VARIATION_SONG_DATABASES_QUERY,
+																											data: variationSongDatabases,
+																											variables: {
+																												variationId:
+																													inputProps.variationId
+																											}
+																										}
+																									);
+																								}
+																							}
+																						}
+																					);
+																				} else {
+																					addVariationToSongDatabase(
+																						{
+																							variables: {
+																								songDatabaseId:
+																									p.id,
+																								variationId:
+																									inputProps.variationId
+																							},
+																							update: (
+																								cache,
+																								res: FetchResult<
+																									addVariationToSongDatabaseMutation
+																								>
+																							) => {
+																								if (
+																									res
+																										.data
+																										.addVariationToSongDatabase
+																								) {
+																									const variationSongDatabases: getVariationSongDatabasesQuery = cache.readQuery(
+																										{
+																											query: VARIATION_SONG_DATABASES_QUERY,
+																											variables: {
+																												variationId:
+																													inputProps.variationId
+																											}
+																										}
+																									);
+
+																									variationSongDatabases.variationSongDatabases.songDatabases.push(
+																										p
+																									);
+
+																									cache.writeQuery(
+																										{
+																											query: VARIATION_SONG_DATABASES_QUERY,
+																											data: variationSongDatabases,
+																											variables: {
+																												variationId:
+																													inputProps.variationId
+																											}
+																										}
+																									);
+																								}
+																							}
+																						}
+																					);
+																				}
+																			}}
+																		/>
+																		{" " +
+																			p.name}
+																	</div>
+																);
+															}
+														)}
 													</div>
-												);
-											}
+												)}
+											</Mutation>
 										)}
-									</div>
+									</Mutation>
 								);
 							}}
 						</Query>
